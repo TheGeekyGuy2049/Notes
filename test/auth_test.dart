9f2c34071bbeb936b782cc8c76e38e1058331a16
TheGeekyGuy2049 <abdelrahman.mohamed2049@gmail.com>
2024-05-11 21:03:42 +0300

    Initial

---
+++ test/auth_test.dart
@@ -0,0 +1,57 @@
+import 'package:notes/Services/Auth/auth_provider.dart';
+import 'package:notes/Services/Auth/auth_user.dart';
+import 'package:test/test.dart';
+
+void main() {
+
+}
+class NotInitializeException implements Exception {
+
+}
+
+class MockAuthProvider implements AuthProvider {
+
+  var _isInitialized = false;
+  bool get isInitialized => _isInitialized;
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  })
+  {
+    if (!isInitialized) throw NotInitializeException();
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> initialize() {
+    // TODO: implement initialize
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+
+}
